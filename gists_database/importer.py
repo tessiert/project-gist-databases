787f9d904fab1af403406a6a5a1101e4831cfbb5
Trace Tessier <tessiert@unm.edu>
2018-09-26 18:04:50 +0000

    Primary tests passing.

--- gists_database/importer.py
+++ gists_database/importer.py
@@ -1,4 +1,44 @@
 import requests
+from requests import exceptions
 
 def import_gists_to_database(db, username, commit=True):
-    pass
+
+    # Get the data for <username>
+    url = 'https://api.github.com/users/{user}/gists'.format(user=username)
+
+    response = requests.get(url)
+    if response.status_code == 404:
+        raise exceptions.HTTPError
+
+    gist_data = response.json()
+
+    # Insert into db
+    insert_cmd = """INSERT INTO gists(
+                        github_id,
+                        html_url,
+                        git_pull_url,
+                        git_push_url,
+                        commits_url,
+                        forks_url,
+                        public,
+                        created_at,
+                        updated_at,
+                        comments,
+                        comments_url)
+                    VALUES(
+                        :id,
+                        :html_url,
+                        :git_pull_url,
+                        :git_push_url,
+                        :commits_url,
+                        :forks_url,
+                        :public,
+                        :created_at,
+                        :updated_at,
+                        :comments,
+                        :comments_url)"""
+    [db.execute(insert_cmd, cur_gist) for cur_gist in gist_data]
+    if (commit):
+        db.commit()
+
+
--- gists_database/search.py
+++ gists_database/search.py
@@ -1,4 +1,26 @@
+from datetime import datetime
 from .models import Gist
 
 def search_gists(db_connection, **kwargs):
-    pass
+
+    cursor = db_connection.execute('SELECT * FROM gists')
+
+    # Return everything if no filters specified
+    if not kwargs:
+        return [Gist(gist) for gist in cursor]
+
+    # Process any filtering constraints passed in kwargs
+    DATE_ATTRS = ['created_at', 'updated_at']
+    gists = []
+
+    for row in cursor:
+        gist = Gist(row)
+        for field, search_value in kwargs.items():
+            field_value = getattr(gist, field)
+            if field in DATE_ATTRS:
+                field_value = datetime.strptime(field_value, '%Y-%m-%dT%H:%M:%SZ')
+            if field_value != search_value:
+                break
+            gists.append(gist)
+
+    return gists
