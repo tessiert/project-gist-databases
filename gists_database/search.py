1c672c137a01c2520be2b28b3ed36283c1b31ac4
Trace Tessier <tessiert@unm.edu>
2018-09-27 21:53:25 +0000

    Project submission - Tessier.  All tests passing.

--- gists_database/search.py
+++ gists_database/search.py
@@ -1,26 +1,80 @@
 from datetime import datetime
 from .models import Gist
 
+
 def search_gists(db_connection, **kwargs):
 
-    cursor = db_connection.execute('SELECT * FROM gists')
+    PARSED_DATE_CRITERION = {
+        'created_at__lt': {
+            'name': 'datetime(created_at)',
+            'operator': '<'
+            },
+        'created_at__lte': {
+            'name': 'datetime(created_at)',
+            'operator': '<='
+            },
+        'created_at__gt': {
+            'name': 'datetime(created_at)',
+            'operator': '>'
+            },
+        'created_at__gte': {
+            'name': 'datetime(created_at)',
+            'operator': '>='
+            },
+        'created_at': {
+            'name': 'datetime(created_at)',
+            'operator': '=='
+            },
+        'updated_at__lt': {
+            'name': 'datetime(updated_at)',
+            'operator': '<'
+            },
+        'updated_at__lte': {
+            'name': 'datetime(updated_at)',
+            'operator': '<='
+            },
+        'updated_at__gt': {
+            'name': 'datetime(updated_at)',
+            'operator': '>'
+            },
+        'updated_at__gte': {
+            'name': 'datetime(updated_at)',
+            'operator': '>='
+            },
+        'updated_at': {
+            'name': 'datetime(updated_at)',
+            'operator': '=='
+            }
+        }
 
-    # Return everything if no filters specified
-    if not kwargs:
-        return [Gist(gist) for gist in cursor]
+    query = 'SELECT * FROM gists WHERE '
 
-    # Process any filtering constraints passed in kwargs
-    DATE_ATTRS = ['created_at', 'updated_at']
-    gists = []
+    params = {}
 
-    for row in cursor:
-        gist = Gist(row)
-        for field, search_value in kwargs.items():
-            field_value = getattr(gist, field)
-            if field in DATE_ATTRS:
-                field_value = datetime.strptime(field_value, '%Y-%m-%dT%H:%M:%SZ')
-            if field_value != search_value:
-                break
-            gists.append(gist)
+    # Process any filtering constraints passed via kwargs
+    count = 0
+    for criterion, search_value in kwargs.items():
+        count += 1
+        if criterion in PARSED_DATE_CRITERION.keys():
+            parsed_criterion = PARSED_DATE_CRITERION[criterion]
+        else:
+            parsed_criterion = {
+                'name': criterion,
+                'operator': '=='
+                }
 
-    return gists
+        search_value_tag = 'search_value_{i}'.format(i=count)
+        query += '{criterion_name} {operator} :{search_value_tag} AND '.format(
+            criterion_name=parsed_criterion['name'],
+            operator=parsed_criterion['operator'],
+            search_value_tag=search_value_tag
+            )
+
+        params.update({search_value_tag: search_value})
+
+    # Clean up hanging bits from query construction
+    query = query.rstrip('WHERE ').rstrip('AND ')
+
+    # Run the query and return a list of Gist objects
+    cursor = db_connection.execute(query, params)
+    return [Gist(gist) for gist in cursor]
